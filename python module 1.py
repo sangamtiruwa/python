4d4b7992109f933e3cb39dd57a26c76e5b530332
sanga <sangamt@metropolia.fi>
2023-09-27 12:57:36 +0300

    New project

---
+++ python module 1.py
@@ -0,0 +1,4 @@
+'''Write a program that greets you by your own name. If your name was Viivi Virta, the output of the program would be Hello, Viivi Virta!.'''
+
+name = input('Enter your name: ')
+print("Hello, " + name + "!")
