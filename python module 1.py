25786e157af477e69243f774d5ff6f1bcc408992
sangamtiruwa <145536188+sangamtiruwa@users.noreply.github.com>
2023-10-14 15:57:04 +0300

    Delete python module 1.py

--- python module 1.py
+++
@@ -1,4 +0,0 @@
-'''Write a program that greets you by your own name. If your name was Viivi Virta, the output of the program would be Hello, Viivi Virta!.'''
-
-name = input('Enter your name: ')
-print("Hello, " + name + "!")
