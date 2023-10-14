8cf95eb21fb866106ee193157a3f0c96da657130
sanga <sangamt@metropolia.fi>
2023-10-14 16:18:55 +0300

    29

---
+++ Python Module 1-2/Pyt.Md1(1.1).py
@@ -0,0 +1,5 @@
+#Write a program that greets you by your own name. If your name was Viivi Virta, the output of
+# the program would be Hello, Viivi Virta!.
+
+name = input("Enter name: ")
+print("Hello, " + name + ("!"))
---
+++ Python Module 1-2/Pyt.Md2(2.3).py
@@ -0,0 +1,10 @@
+#3. Write a program that asks the user for the length and width of a rectangle.
+# The program then prints out the perimeter and area of the rectangle. The perimeter of a rectangle is the
+# sum of the lengths of each four sides.
+
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+perimeter = 2 * (length + width)
+area = length * width
+print(f"The perimeter of the rectangle {perimeter: .2f}")
+print(f"Area of the rectangle: {area: .2f}")
